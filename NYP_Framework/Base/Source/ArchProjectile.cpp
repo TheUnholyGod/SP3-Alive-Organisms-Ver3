d3f817037174dfe673d728330cb195d37d4706b6
Chee Mun <hoopawolf394@gmail.com>
2017-08-28 16:20:44 +0800

    projectile
    projectile

--- NYP_Framework/Base/Source/ArchProjectile.cpp
+++ NYP_Framework/Base/Source/ArchProjectile.cpp
@@ -1,6 +1,29 @@
 #include "ArchProjectile.h"
 
+#include "PlayerInfo\PlayerInfo.h"
+#include "MapManager.h"
+
 void ArchProjectile::Update(double dt)
 {
 	position.y -= m_gravity * dt;
+
+	if (!m_active)
+		return;
+	this->position += velocity * dt;
+	this->tile_ID = MapManager::GetInstance()->GetLevel(Player::GetInstance()->GetCurrentLevel())->ReturnTileViaPos(this->position, Player::GetInstance()->GetIsFightingBoss());
+	this->GenerateAABB(this->position);
+
+	if (position.y < 0)
+	{
+		this->SetIsDone(true);
+		this->m_active = false;
+		this->m_bCollider = false;
+		this->tile_ID = -1;
+	}
+}
+
+bool ArchProjectile::CollisionResponse(GenericEntity * ThatEntity)
+{
+
+	return false;
 }
