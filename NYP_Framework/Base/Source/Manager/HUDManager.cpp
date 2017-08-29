9ee623329ffb31c799d37d60dbb01dd35c777a8e
Lin Xin <calipaolx@gmail.com>
2017-08-29 11:10:47 +0800

    fixed hud bar not scaling to screen size

--- NYP_Framework/Base/Source/Manager/HUDManager.cpp
+++ NYP_Framework/Base/Source/Manager/HUDManager.cpp
@@ -8,27 +8,27 @@ void HUDManager::RenderHUD()
 	MS& modelStack = GraphicsManager::GetInstance()->GetModelStack();
 	modelStack.PushMatrix();
 	modelStack.Translate(Application::GetInstance().GetWindowWidth() * -0.4 - (100 - HPsizeX), Application::GetInstance().GetWindowHeight() * 0.45, 0);
-	modelStack.Scale(HPsizeX * 5, 500, 1);
+	modelStack.Scale(Application::GetInstance().GetWindowWidth() * 0.4 * HPsizeX/100, Application::GetInstance().GetWindowHeight() * 0.4, 1);
 	RenderHelper::RenderMesh(MeshList::GetInstance()->GetMesh("health_bar"));
 	modelStack.PopMatrix();
 
 	//HP
-	modelStack.PushMatrix();
-	modelStack.Translate(Application::GetInstance().GetWindowWidth() *0.0000011 /*- (100 - HPsizeX)*/, Application::GetInstance().GetWindowHeight() * 0.45, 0);
-	modelStack.Scale(50, 50, 1);
-	RenderHelper::RenderMesh(MeshList::GetInstance()->GetMesh("HP"));
-	modelStack.PopMatrix();
+	//modelStack.PushMatrix();
+	//modelStack.Translate(Application::GetInstance().GetWindowWidth() *0.0000011, Application::GetInstance().GetWindowHeight() * 0.45, 0);
+	//modelStack.Scale(Application::GetInstance().GetWindowWidth() * 0.1, Application::GetInstance().GetWindowHeight() * 0.1, 1);
+	//RenderHelper::RenderMesh(MeshList::GetInstance()->GetMesh("HP"));
+	//modelStack.PopMatrix();
 
 	//WEAPONS
 	modelStack.PushMatrix();
 	modelStack.Translate(Application::GetInstance().GetWindowWidth() *-0.26, Application::GetInstance().GetWindowHeight()* 0.30, 6);
-	modelStack.Scale(100, 100, 1);
+	modelStack.Scale(Application::GetInstance().GetWindowWidth() * 0.1, Application::GetInstance().GetWindowHeight() * 0.1, 1);
 	RenderHelper::RenderMesh(MeshList::GetInstance()->GetMesh("bow"));
 	modelStack.PopMatrix();
 
 	modelStack.PushMatrix();
 	modelStack.Translate(Application::GetInstance().GetWindowWidth() *-0.43, Application::GetInstance().GetWindowHeight()* 0.30, 6);
-	modelStack.Scale(100, 100, 1);
+	modelStack.Scale(Application::GetInstance().GetWindowWidth() * 0.1, Application::GetInstance().GetWindowHeight() * 0.1, 1);
 	RenderHelper::RenderMesh(MeshList::GetInstance()->GetMesh("sword"));
 	modelStack.PopMatrix();
 
@@ -36,7 +36,7 @@ void HUDManager::RenderHUD()
 	//HOTBAR
 	modelStack.PushMatrix();
 	modelStack.Translate(Application::GetInstance().GetWindowWidth() *- 0.345, Application::GetInstance().GetWindowHeight()* 0.30, 5);
-	modelStack.Scale(250, 250, 1);
+	modelStack.Scale(Application::GetInstance().GetWindowWidth() * 0.3, Application::GetInstance().GetWindowHeight() * 0.3, 1);
 	RenderHelper::RenderMesh(MeshList::GetInstance()->GetMesh("hot_bar"));
 	modelStack.PopMatrix();
 
